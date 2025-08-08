f395336d83ca8c4cb4f142af23597a0ea9940a35
aron <>
2025-08-08 07:10:19 +0000

    a

--- dicedistributions/dicedistributions.py
+++ dicedistributions/dicedistributions.py
@@ -4,14 +4,24 @@ from math import gcd
 from functools import reduce
 import sys
 
+import time
+
+t0 = time.perf_counter()
+
 A, B, C = [list(map(int, line.split())) for line in sys.stdin.read().splitlines()[1:]]
 
-sums = Counter(a+b for a in A for b in B)
+dt = time.perf_counter() - t0
+print(f"{dt * 1000:.3f}ms\tIO")
+t0 = time.perf_counter()
 
+sums = Counter(a+b for a in A for b in B)
 known_die = Counter(C)
 min_known = min(known_die)
-
 new_die = Counter()
+
+dt = time.perf_counter() - t0
+print(f"{dt * 1000:.3f}ms\tSetup of vars")
+t0 = time.perf_counter()
 
 while sums:
     s = min(sums)
@@ -28,19 +38,27 @@ while sums:
             if sums[t] == 0:
                 del sums[t]
 
-print(new_die)
-print(f"number\toccurence")
-for key in new_die.keys():
-    print(f"{key}\t{new_die[key]}")
+dt = time.perf_counter() - t0
+print(f"{dt * 1000:.3f}ms\tLoop to generate new die faces prototype")
+t0 = time.perf_counter()
 
-print("")
 denom = 1
-for v in new_die.values():
-    denom = denom * v.denominator // gcd(denom, v.denominator)
+for value in new_die.values():
+    denom = denom * value.denominator // gcd(denom, value.denominator)
+
+dt = time.perf_counter() - t0
+print(f"{dt * 1000:.3f}ms\tExtracting denominators")
+t0 = time.perf_counter()
 
 faces = []
 for face, v in new_die.items():
     faces.extend([face] * int(v * denom))
+
+dt = time.perf_counter() - t0
+print(f"{dt * 1000:.3f}ms\tTime for making new faces list:  ")
+t0 = time.perf_counter()
+
+
 
 g = 0
 for cnt in Counter(faces).values():
@@ -54,6 +72,20 @@ if g > 1:
 
         faces = sorted(reduced)
 
+dt = time.perf_counter() - t0
+print(f"{dt * 1000:.3f}ms\treducing list for uneccessary dupblicates")
+t0 = time.perf_counter()
 
 print(len(faces))
 print(' '.join(map(str, faces)))
+
+
+# (base) aroncull100@23a737045eaa:~/work/kattis/dicedistributions$ python dicedistributions.py < dicedistributions1.in 
+# 0.054ms IO
+# 0.071ms Setup of vars
+# 0.361ms Loop to generate new die faces prototype
+# 0.008ms Extracting denominators
+# 0.044ms Time for making new faces list:  
+# 0.014ms reducing list for uneccessary dupblicates
+# 6
+# 1 2 3 4 5 6
